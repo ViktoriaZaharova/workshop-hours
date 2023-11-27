5767512ca773bb851e832a5337347d21d185881a
Виктория Захарова <zaharova181114@gmail.com>
2023-11-27 23:23:47 +0200

    all

--- js/common.js
+++ js/common.js
@@ -80,3 +80,18 @@ $('.brands-slider').slick({
 
 	]
 });
+
+// accordeon
+function accordeon() {
+	var panel = $('.panel_heading');
+
+	if (panel.hasClass('in')) {
+		$('.in').find('.block_hover').slideDown();
+	}
+
+	$('.panel_heading .block_title').on('click', function () {
+		$(this).parent().toggleClass('in').find('.block_hover').slideToggle();
+	});
+}
+
+accordeon();
